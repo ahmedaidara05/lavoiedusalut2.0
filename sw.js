45e44cbc194ce275f170041eef043ce5bdf4450a
ahmedaidara05 <ahmedsaidaidara5@gmail.com>
2025-05-31 00:29:24 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+                '/payment.js',
+                '/logo.png',
+                '/cover.jpg'
+            ]).catch(err => console.error('Cache installation failed:', err));
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).then((networkResponse) => {
+                if (event.request.url.startsWith('http')) {
+                    caches.open('v1').then((cache) => {
+                        cache.put(event.request, networkResponse.clone());
+                    });
+                }
+                return networkResponse;
+            }).catch(() => caches.match('/index.html'));
+        })
+    );
+});
